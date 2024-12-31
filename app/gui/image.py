dad3192c022b2050b64f6fe0df632432a96bbc0d
LionRouge1 <matchoudiavlessi@gmail.com>
2024-12-31 17:54:00 +0000

    Complete all the main functionalities

--- app/gui/image.py
+++ app/gui/image.py
@@ -1,21 +1,61 @@
-import requests
-from io import BytesIO
-from PIL import Image
-import re
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from ..core.image import ImageData
 
-image_url = "https://sdbooth2-production.s3.amazonaws.com/hm664gvuunhkdnowt0i81lt0bp6o"
-response = requests.get(image_url)
-response.raise_for_status()
-content_disposition = response.headers.get('Content-Disposition')
-if content_disposition:
-  filename = re.findall('filename="(.+)"', content_disposition)
-  if filename:
-    image_name = filename[0]
-  else:
-    image_name = "unknown"
-else:
-  # Fallback to extracting the filename from the URL
-  image_name = image_url.split("/")[-1]
-image_data = BytesIO(response.content)
-img = Image.open(image_data)
-print(filename, type(img.format), img.size)
+from PyQt6.QtWidgets import *
+
+class ImageWidget(QWidget):
+  def __init__(self, url):
+    super().__init__()
+    self.url = url
+    self.visibility_state = False
+    self.image = ImageData(url)
+    self.pixmap = QPixmap()
+    self.pixmap.loadFromData(self.image.display_image())
+    self.image_label = QLabel()
+    self.image_label.setPixmap(self.pixmap)
+    self.image_label.setScaledContents(True)
+    self.toggle_button = QPushButton("Show Details")
+    self.toggle_button.setCursor(Qt.CursorShape.PointingHandCursor)
+    self.save_button = QPushButton("Save Image")
+    self.save_button.setCursor(Qt.CursorShape.PointingHandCursor)
+    self.save_button.setStyleSheet("background: green; color: white")
+    self.save_button.clicked.connect(self.save_image)
+    self.toggle_button.clicked.connect(self.toggle_details)
+    self.image_box_layout = QVBoxLayout()
+    self.button_widget = QWidget()
+    self.button_layout = QHBoxLayout()
+    self.button_layout.addStretch()
+    self.button_widget.setLayout(self.button_layout)
+    self.button_layout.addWidget(self.toggle_button)
+    self.button_layout.addWidget(self.save_button)
+
+    self.details_label = QLabel()
+    self.image_properties = self.image.image_properties()
+    self.details_label.setText(
+      f"Image Name: {self.image_properties['filename']}\n"
+      f"Image Size: {self.image_properties['size']}\n"
+      f"Image Format: {self.image_properties['format']}\n"
+      f"File Size: {self.image_properties['file_size']} MB\n"
+    )
+    self.details_label.setVisible(self.visibility_state)
+    self.image_box_layout.addWidget(self.image_label)
+    self.image_box_layout.addWidget(self.button_widget)
+    self.image_box_layout.addWidget(self.details_label)
+    self.setLayout(self.image_box_layout)
+
+  def toggle_details(self):
+    self.visibility_state = not self.visibility_state
+    self.details_label.setVisible(self.visibility_state)
+    self.toggle_button.setText("Hide Details" if self.visibility_state else "Show Details")
+
+  def save_image(self):
+    self.image.save_image()
+
+    msg_box = QMessageBox()
+    msg_box.setIcon(QMessageBox.Icon.Information)
+    msg_box.setText("Image has been successfully downloaded.")
+    msg_box.setWindowTitle("Download Complete")
+    msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
+    msg_box.exec()
+
--- app/gui/images.py
+++ app/gui/images.py
@@ -1,28 +1,9 @@
-import sys
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from ..core.website_content import Content
-from ..core.image import ImageData
+from .image import ImageWidget
 
 from PyQt6.QtWidgets import *
-
-def download_all_images(urls):
-  for url in urls:
-    image = ImageData(url)
-    image.save_image()
-  
-  msg_box = QMessageBox()
-  msg_box.setIcon(QMessageBox.Icon.Information)
-  msg_box.setText("All images have been successfully downloaded.")
-  msg_box.setWindowTitle("Download Complete")
-  msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
-  msg_box.exec()
-
-def save_image_fn(url):
-  print(url)
-  img = ImageData(url)
-  return img.save_image()
-    
 
 class ImagesWindow(QWidget):
   def __init__(self, url):
@@ -30,6 +11,7 @@ class ImagesWindow(QWidget):
     if self.parent():
       self.parent().layout().removeWidget(self)
 
+    self.images = []
     self.label = QLabel("Here are the images from the website:")
     download_all = QPushButton("Download All")
     download_all.setStyleSheet("background: green; color: white")
@@ -64,7 +46,7 @@ class ImagesWindow(QWidget):
 
     web_content = Content(url)
     images_url = web_content.get_images()
-    download_all.clicked.connect(lambda: download_all_images(images_url))
+    download_all.clicked.connect(self.download_all_images)
 
     grid_layout = QGridLayout()
     images_widget.setLayout(grid_layout)
@@ -72,58 +54,21 @@ class ImagesWindow(QWidget):
     row, col = 0, 0
 
     for url in images_url:
-      image_box = QWidget()
-      image_box_layout = QVBoxLayout()
-      image_box.setLayout(image_box_layout)
-
-      image_label = QLabel()
-      image = ImageData(url)
-      pixmap = QPixmap()
-      pixmap.loadFromData(image.display_image())
-      image_label.setPixmap(pixmap)
-      image_label.setScaledContents(True)
-      image_box_layout.addWidget(image_label)
-
-      button_box = QWidget()
-      button_layout = QHBoxLayout()
-      button_layout.addStretch()
-      button_box.setLayout(button_layout)
-
-      toggle_button = QPushButton("Show Details")
-      button_layout.addWidget(toggle_button)
-      # image_box_layout.addWidget(toggle_button)
-
-      save_button = QPushButton("Save Image")
-      button_layout.addWidget(save_button)
-      save_button.setStyleSheet("background: green; color: white")
-      # save_button.clicked.connect(lambda url=url: ImageData(url).save_image())
-      image_box_layout.addWidget(button_box)
-
-      details_label = QLabel()
-      image_properties = image.image_properties()
-      details_label.setText(
-        f"Image Name: {image_properties['filename']}\n"
-        f"Image Size: {image_properties['size']}\n"
-        f"Image Format: {image_properties['format']}\n"
-        f"File Size: {image_properties['file_size']} MB\n"
-      )
-      details_label.setVisible(False)
-      image_box_layout.addWidget(details_label)
-
-      def create_toggle_details_fn(label, button):
-        visibility_state = False
-        def toggle_details():
-          nonlocal visibility_state
-          visibility_state = not visibility_state
-          label.setVisible(visibility_state)
-          button.setText("Hide Details" if visibility_state else "Show Details")
-        return toggle_details
-
-      toggle_button.clicked.connect(create_toggle_details_fn(details_label, toggle_button))
-      save_button.clicked.connect(lambda url=url: save_image_fn(url))
-      
-      grid_layout.addWidget(image_box, row, col)
+      image_widget = ImageWidget(url)
+      self.images.append(image_widget.image)
+      grid_layout.addWidget(image_widget, row, col)
       col += 1
       if col == 5:
         row += 1
         col = 0
+
+  def download_all_images(self):
+    for image in self.images:
+      image.save_image()
+
+    msg_box = QMessageBox()
+    msg_box.setIcon(QMessageBox.Icon.Information)
+    msg_box.setText("All images have been successfully downloaded.")
+    msg_box.setWindowTitle("Download Complete")
+    msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
+    msg_box.exec()
