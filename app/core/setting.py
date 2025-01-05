8c9cd793ab3b2bd3c007ea0961f02d7b5567078a
LionRouge1 <matchoudiavlessi@gmail.com>
2025-01-05 16:06:34 +0000

    Implement setting functionality

--- app/core/setting.py
+++ app/core/setting.py
@@ -1,11 +1,28 @@
 import os
+from pathlib import Path
+import json
 
 class Settings:
-  def __init__(self, save_directory=None, image_format='jpg', max_images=100):
-    self.save_directory = save_directory or os.path.join(os.getcwd(), 'images')
-    self.image_format = image_format
-    self.max_images = max_images
-    self.get_css_images = False
+  def __init__(self):
+    self.default_directory = os.path.join(self.get_download_folder(), "downloader_images")
+    self.save_directory = self.default_directory
+    self.image_format = ''
+    self.max_images = 50
+    self.get_css_images = True
+    self.load_settings()
+
+  def load_settings(self):
+    if os.path.exists('settings.json'):
+      with open('settings.json', 'r') as json_file:
+        settings_dict = json.load(json_file)
+        self.save_directory = settings_dict.get('save_directory', self.default_directory)
+        self.image_format = settings_dict.get('image_format', 'jpg')
+        self.max_images = settings_dict.get('max_images', 50)
+        self.get_css_images = settings_dict.get('get_css_images', False)
+
+  def create_save_directory(self):
+    if not os.path.exists(self.get_download_folder()):
+      os.makedirs(self.default_directory, exist_ok=True)
 
   def update_save_directory(self, new_directory):
     if os.path.exists(new_directory):
@@ -27,6 +44,25 @@ class Settings:
     
   def enable_css_images(self):
     self.get_css_images = True
+
+  def get_download_folder(self):
+    """Get the default Downloads folder for the current OS."""
+    if os.name == 'nt':  # Windows
+      return str(Path.home() / "Downloads")
+    elif os.name == 'posix':  # macOS or Linux
+      return str(Path.home() / "Downloads")
+    else:
+      raise NotImplementedError("OS not supported")
+    
+  def save_settings(self, new_directory, max_images, get_css_images):
+    settings_dict = {
+      'save_directory': new_directory,
+      'image_format': self.image_format,
+      'max_images': max_images,
+      'get_css_images': get_css_images
+    }
+    with open('settings.json', 'w') as json_file:
+      json.dump(settings_dict, json_file, indent=2)
 
 # Example usage
 # settings = Settings()
--- app/gui/setting.py
+++ app/gui/setting.py
@@ -1,43 +1,96 @@
 import sys
+from PyQt6.QtGui import QIntValidator
 
 from PyQt6.QtWidgets import (
-  QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QCheckBox, QFileDialog
+  QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QCheckBox, QFileDialog, QGridLayout
 )
+from ..core.setting import Settings
 
 class SettingsUI(QWidget):
   def __init__(self):
     super().__init__()
-    self.initUI()
-
-  def initUI(self):
+    self.settings = Settings()
     layout = QVBoxLayout()
+    layout.addStretch()
+    self.setLayout(layout)
+    self.params_widget = QWidget()
+    params_layout = QGridLayout()
+    self.params_widget.setLayout(params_layout)
 
     # Directory selection
     self.dir_label = QLabel('Select Directory to Save Images:')
-    layout.addWidget(self.dir_label)
+    params_layout.addWidget(self.dir_label, 0, 0)
 
     self.dir_line_edit = QLineEdit(self)
-    layout.addWidget(self.dir_line_edit)
+    params_layout.addWidget(self.dir_line_edit, 0, 1)
+    self.dir_line_edit.setText(self.settings.save_directory)
+    self.dir_line_edit.setReadOnly(True)
 
     self.dir_button = QPushButton('Browse', self)
     self.dir_button.clicked.connect(self.select_directory)
-    layout.addWidget(self.dir_button)
+    params_layout.addWidget(self.dir_button, 0, 2)
+
+
+    # Max images
+    self.max_image_label = QLabel("Max Images per request:")
+    params_layout.addWidget(self.max_image_label, 1, 0)
+
+    self.max_image_line_edit = QLineEdit(self)
+    self.max_image_line_edit.setValidator(QIntValidator(0, 999, self))
+    self.max_image_line_edit.setText(str(self.settings.max_images))
+    params_layout.addWidget(self.max_image_line_edit, 1, 1)
+
+    self.max_image_description_label = QLabel('Specify the maximum number of images to download per request.')
+    self.max_image_description_label.setWordWrap(True)
+    self.max_image_description_label.setStyleSheet('''
+      color: grey;
+      font-size: 12px;
+    ''')
+    params_layout.addWidget(self.max_image_description_label, 2, 1)
 
     # Enable CSS images
-    self.css_checkbox = QCheckBox('Enable CSS Images', self)
-    layout.addWidget(self.css_checkbox)
+    self.enable_css_images_label = QLabel('Enable CSS Images:')
+    self.enable_css_images_label.mousePressEvent = lambda event: self.css_checkbox.toggle()
+    params_layout.addWidget(self.enable_css_images_label, 3, 0)
+    
+    self.css_checkbox = QCheckBox(self)
+    self.css_checkbox.setChecked(self.settings.get_css_images)
+    self.css_checkbox.setStyleSheet('''
+      QCheckBox::indicator {
+      width: 15px;
+      height: 15px;
+      };
+    ''')
+    params_layout.addWidget(self.css_checkbox, 3, 1)
+
+    # Description for CSS images checkbox
+    self.css_description_label = QLabel('When checked, CSS images will be downloaded along with the main images.')
+    self.css_description_label.setWordWrap(True)
+    self.css_description_label.setStyleSheet('''
+      color: grey;
+      font-size: 12px;
+    ''')
+    params_layout.addWidget(self.css_description_label, 4, 1)
 
     # Save button
     self.save_button = QPushButton('Save Settings', self)
+    self.save_button.clicked.connect(self.save_setting)
+    layout.addWidget(self.params_widget)
     layout.addWidget(self.save_button)
 
-    self.setLayout(layout)
     self.setWindowTitle('Settings')
 
   def select_directory(self):
     directory = QFileDialog.getExistingDirectory(self, 'Select Directory')
     if directory:
       self.dir_line_edit.setText(directory)
+
+  def save_setting(self):
+    directory = self.dir_line_edit.text()
+    max = self.max_image_line_edit.text()
+    get_css = self.css_checkbox.isChecked()
+
+    self.settings.save_settings(directory, max, get_css)
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
